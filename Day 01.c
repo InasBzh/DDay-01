7915aa654bcb191b57f37ae3460f0afb047cb7eb
InasBzh <i.benzahra11@gmail.com>
2025-09-24 09:56:31 +0100

    Nouveau Day01

---
+++ Day 01.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+int main()
+{
+     char Nom [38];
+     char Prenom [38];
+     int Age;
+     char Sexe [38];
+     char AdresseEmail [38];
+    
+    printf ("Votre Nom:");
+    scanf ("%c" ,&Nom );
+    
+     printf ("Votre Prenom: \n");
+    scanf ("%c" ,&Prenom );
+    
+     printf ("Votre Age: \n");
+    scanf ("%d" ,&Age );
+    
+    printf ("Votre Sexe: \n");
+    scanf ("%c" ,&Sexe );
+    
+     printf ("Votre Adresse Email: \n");
+     scanf ("%c" ,&AdresseEmail);
+}
